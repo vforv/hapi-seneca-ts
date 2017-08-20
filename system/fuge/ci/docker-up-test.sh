02b7d66ee73a6514d1ab73ad621191e54149a605
Vladimir Djukic <vladimirdjukic90@gmail.com>
2017-08-20 17:27:04 +0200

    Script fix

--- system/fuge/ci/docker-up-test.sh
+++ system/fuge/ci/docker-up-test.sh
@@ -9,4 +9,5 @@ mkdir -p $HOME/.cache/yarn
 mkdir -p $HOME/.nuget
 
 docker-compose -f docker-compose-test.yml -p dev-test up -d --build
+docker-compose -p dev-test logs -f &
 exit 0
--- system/fuge/ci/docker-up.sh
+++ system/fuge/ci/docker-up.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-set -o errexit
 
 cd $(dirname $0)
 cd ../
