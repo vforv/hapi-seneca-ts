e15be957b2f3d9fcc26819460aedf2b44542ad8b
Zeljko Radic <zradic@yahoo.com>
2017-08-23 18:29:43 +0200

    repl

--- repl/repl.ts
+++ repl/repl.ts
@@ -1,9 +1,40 @@
 import * as Seneca from 'seneca';
 const repl = require('seneca-repl');
 
+const HOST = process.env.HOST;
+const BROADCAST = process.env.BROADCAST;
+const REGISTRY = process.env.REGISTRY;
+
 Seneca({ tag: 'repl' })
   // .test('print')
-  .use('mesh')
+  .use('consul-registry', {
+    host: REGISTRY,
+    port: "8500"
+  })
+  .use('mesh', {
+    auto: true,
+    host: HOST,
+    // bases: [`${BASES}:39999`],
+    discover: {
+      // multicast: {
+      //   address: BROADCAST
+      // },
+      // registry: {
+      //   host: '127.0.0.1',
+      //   port: "39999"
+      // }
+      registry: {
+        active: true
+      }
+    },
+    dumpnet: false,
+    // listen: [
+    //   {
+    //     pin: 'role:ping,cmd:date',
+    //     host: HOST
+    //   }
+    // ]
+  })
   .use(repl)
   .ready(() => {
     console.log("REPL service ready!!!")
