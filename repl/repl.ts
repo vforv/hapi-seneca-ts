b2e4a4f6e6dedc0fd1902994ea0422b16140c8ec
Zeljko Radic <zradic@yahoo.com>
2017-08-24 17:50:00 +0200

    fix

--- repl/repl.ts
+++ repl/repl.ts
@@ -5,6 +5,7 @@ const HOST = process.env.HOST;
 const HOST1 = process.env.HOST1;
 const REGISTRY = process.env.REGISTRY;
 const BASES = process.env.BASES;
+const PORT = process.env.PORT;
 
 Seneca({ tag: 'repl' })
   .use('mesh', {
@@ -15,7 +16,7 @@ Seneca({ tag: 'repl' })
   })
   .use(repl, {
     host: HOST1,
-    port: '10001'
+    port: PORT
   })
   .ready(() => {
     console.log("REPL service ready!!!")
